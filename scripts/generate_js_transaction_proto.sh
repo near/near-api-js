c40ead4cfaa915c181411ada20a6dfdfe7024651
Vladimir Grichina <vgrichina@gmail.com>
2019-05-03 23:54:06 -0700

    Add back and fix proto generation script

---
+++ scripts/generate_js_transaction_proto.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+NEARLIB_DIR="$(npm prefix)"
+NEARCORE_DIR="${NEARCORE_DIR:-../nearcore}"
+"$(npm bin)"/pbjs \
+	-t static-module \
+	-w commonjs \
+	-o ${NEARLIB_DIR}/protos.js \
+	${NEARCORE_DIR}/runtime/protos/protos/signed_transaction.proto
