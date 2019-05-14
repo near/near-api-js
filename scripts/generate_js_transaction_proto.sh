1fc37a26b084a6b8487ed6be44bd8c3cd891e943
Vladimir Grichina <vgrichina@gmail.com>
2019-05-14 16:41:12 -0700

    Add debugging into build script

--- scripts/generate_js_transaction_proto.sh
+++ scripts/generate_js_transaction_proto.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -ex
+printenv
 NEARLIB_DIR="$(npm prefix)"
 NEARCORE_DIR="${NEARCORE_DIR:-../nearcore}"
+echo "NEARCORE_DIR ${NEARLIB_DIR}"
 "$(npm bin)"/pbjs \
 	-t static-module \
 	-w commonjs \
