e690c26a4993651e0c3d14c7c4582e450389fc72
Vladimir Grichina <vgrichina@gmail.com>
2019-05-16 19:49:54 -0700

    Revert "Add debugging into build script"
    This reverts commit 1fc37a26b084a6b8487ed6be44bd8c3cd891e943.

--- scripts/generate_js_transaction_proto.sh
+++ scripts/generate_js_transaction_proto.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -ex
-printenv
 NEARLIB_DIR="$(npm prefix)"
 NEARCORE_DIR="${NEARCORE_DIR:-../nearcore}"
-echo "NEARCORE_DIR ${NEARLIB_DIR}"
 "$(npm bin)"/pbjs \
 	-t static-module \
 	-w commonjs \
