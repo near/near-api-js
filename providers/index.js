08237a4ba7dfe9bef62e1237ca548999c62104e8
Illia Polosukhin <ilblackdragon@gmail.com>
2019-06-05 10:56:15 -0700

    Cleanup TS generated files, switch back to pbjs/pbts for proto generations as @vgrichina suggested.

--- providers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const provider_1 = require("./provider");
-exports.Provider = provider_1.Provider;
-const json_rpc_provider_1 = require("./json-rpc-provider");
-exports.JsonRpcProvider = json_rpc_provider_1.JsonRpcProvider;
