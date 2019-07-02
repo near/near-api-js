2d67b3cb1677f8d9fbb90bc9cf4e8c61b974a10e
Vladimir Grichina <vgrichina@gmail.com>
2019-07-02 11:15:22 -0700

    Update test/config.js based on near-shell src/config.js

--- test/config.js
+++ test/config.js
@@ -3,13 +3,27 @@ module.exports = function getConfig(env) {
     case 'production':
     case 'development':
         return {
-            nodeUrl: 'http://34.94.13.241:3030',
+            networkId: 'default',
+            nodeUrl: 'http://34.94.33.164:3030',
             masterAccount: 'test.near',
         };
     case 'local':
+        return {
+            networkId: 'local',
+            nodeUrl: 'http://localhost:3030',
+            keyPath: '~/.near/validator_key.json',
+        };
     case 'test':
         return {
+            networkId: 'local',
             nodeUrl: 'http://localhost:3030',
+            masterAccount: 'test.near',
+        };
+    case 'test-remote':
+    case 'ci':
+        return {
+            networkId: 'shared-test',
+            nodeUrl: 'http://34.94.13.241:3030',
             masterAccount: 'test.near',
         };
     default:
