7864c63c7a1298a2a0cfec3a12061a2333259f1f
Vlad Frolov <frolvlad@gmail.com>
2019-07-10 11:08:28 +0300

    yarn dist

--- lib/providers/json-rpc-provider.d.ts
+++ lib/providers/json-rpc-provider.d.ts
@@ -1,4 +1,4 @@
-import { Provider, FinalTransactionResult } from './provider';
+import { Provider, FinalTransactionResult, NodeStatusResult, BlockResult } from './provider';
 import { Network } from '../utils/network';
 import { ConnectionInfo } from '../utils/web';
 import { SignedTransaction } from '../protos';
@@ -6,8 +6,10 @@ export declare class JsonRpcProvider extends Provider {
     readonly connection: ConnectionInfo;
     constructor(url?: string, network?: Network);
     getNetwork(): Promise<Network>;
+    status(): Promise<NodeStatusResult>;
     sendTransaction(signedTransaction: SignedTransaction): Promise<FinalTransactionResult>;
     txStatus(txHash: Uint8Array): Promise<FinalTransactionResult>;
     query(path: string, data: string): Promise<any>;
+    block(height: number): Promise<BlockResult>;
     private sendJsonRpc;
 }
--- lib/providers/provider.d.ts
+++ lib/providers/provider.d.ts
@@ -1,5 +1,18 @@
 import { Network } from '../utils/network';
 import { SignedTransaction } from '../protos';
+export interface SyncInfo {
+    latest_block_hash: string;
+    latest_block_height: number;
+    latest_block_time: string;
+    latest_state_root: string;
+    syncing: boolean;
+}
+export interface NodeStatusResult {
+    chain_id: string;
+    rpc_addr: string;
+    sync_info: SyncInfo;
+    validators: string[];
+}
 export declare enum FinalTransactionStatus {
     Unknown = "Unknown",
     Started = "Started",
@@ -16,10 +29,36 @@ export interface FinalTransactionResult {
     status: FinalTransactionStatus;
     logs: TransactionLog[];
 }
+export interface TotalWeight {
+    num: number;
+}
+export interface BlockHeader {
+    approval_mask: number[];
+    approval_sigs: number[];
+    hash: number[];
+    height: number;
+    prev_hash: number[];
+    prev_state_root: number[];
+    timestamp: number;
+    total_weight: TotalWeight;
+    tx_root: number[];
+}
+export interface Transaction {
+    hash: number[];
+    public_key: number[];
+    signature: number[];
+    body: any;
+}
+export interface BlockResult {
+    header: BlockHeader;
+    transactions: Transaction[];
+}
 export declare abstract class Provider {
     abstract getNetwork(): Promise<Network>;
+    abstract status(): Promise<NodeStatusResult>;
     abstract sendTransaction(signedTransaction: SignedTransaction): Promise<FinalTransactionResult>;
     abstract txStatus(txHash: Uint8Array): Promise<FinalTransactionResult>;
     abstract query(path: string, data: string): Promise<any>;
+    abstract block(height: number): Promise<BlockResult>;
 }
 export declare function getTransactionLastResult(txResult: FinalTransactionResult): any;
