2c7b49bd003e6e9df3bf0288f7262622999aeeb2
Vlad Frolov <frolvlad@gmail.com>
2019-07-15 19:55:36 +0300

    Added tests

--- test/providers.test.js
+++ test/providers.test.js
@@ -1,6 +1,20 @@
 
 const nearlib = require('../lib/index');
 
+test('json rpc fetch node status', async () => {
+    const config = Object.assign(require('./config')(process.env.NODE_ENV || 'test'));
+    const provider = new nearlib.providers.JsonRpcProvider(config.nodeUrl);
+    let response = await provider.status();
+    expect(response.chain_id).toContain('test-chain');
+});
+
+test('json rpc fetch block info', async () => {
+    const config = Object.assign(require('./config')(process.env.NODE_ENV || 'test'));
+    const provider = new nearlib.providers.JsonRpcProvider(config.nodeUrl);
+    let response = await provider.block(1);
+    expect(response.header.height).toEqual(1);
+});
+
 test('json rpc query account', async () => {
     const config = Object.assign(require('./config')(process.env.NODE_ENV || 'test'));
     const provider = new nearlib.providers.JsonRpcProvider(config.nodeUrl);
