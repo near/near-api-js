bcf8dd85012cc198e8a90a49ed813d1dcd22ac0a
Vladimir Grichina <vgrichina@gmail.com>
2019-07-22 14:22:59 -0700

    yarn dist

--- docs/interfaces/_providers_provider_.finaltransactionresult.md
+++ docs/interfaces/_providers_provider_.finaltransactionresult.md
@@ -12,7 +12,7 @@
 
 **● logs**: *[TransactionLog](_providers_provider_.transactionlog.md)[]*
 
-*Defined in [providers/provider.ts:22](https://github.com/nearprotocol/nearlib/blob/7050e25/src.ts/providers/provider.ts#L22)*
+*Defined in [providers/provider.ts:22](https://github.com/nearprotocol/nearlib/blob/8de97a4/src.ts/providers/provider.ts#L22)*
 
 ___
 <a id="status"></a>
@@ -21,7 +21,7 @@ ___
 
 **● status**: *[FinalTransactionStatus](../enums/_providers_provider_.finaltransactionstatus.md)*
 
-*Defined in [providers/provider.ts:21](https://github.com/nearprotocol/nearlib/blob/7050e25/src.ts/providers/provider.ts#L21)*
+*Defined in [providers/provider.ts:21](https://github.com/nearprotocol/nearlib/blob/8de97a4/src.ts/providers/provider.ts#L21)*
 
 ___
 
--- lib/account.d.ts
+++ lib/account.d.ts
@@ -25,7 +25,7 @@ export declare class Account {
     sendMoney(receiver: string, amount: BN): Promise<FinalTransactionResult>;
     createAccount(newAccountId: string, publicKey: string, amount: BN): Promise<FinalTransactionResult>;
     deployContract(data: Uint8Array): Promise<FinalTransactionResult>;
-    functionCall(contractId: string, methodName: string, args: any): Promise<FinalTransactionResult>;
+    functionCall(contractId: string, methodName: string, args: any, amount?: BN): Promise<FinalTransactionResult>;
     addKey(publicKey: string, contractId?: string, methodName?: string, balanceOwner?: string, amount?: BN): Promise<FinalTransactionResult>;
     deleteKey(publicKey: string): Promise<FinalTransactionResult>;
     stake(publicKey: string, amount: BN): Promise<FinalTransactionResult>;
