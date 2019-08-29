89fd2db87abb280764d345ed540bae65e7da2a1b
Jane <jane.degtiareva@gmail.com>
2019-08-29 13:25:30 -0700

    Fix lint errors in config.js + update lib files

--- test/config.js
+++ test/config.js
@@ -1,38 +1,38 @@
 module.exports = function getConfig(env) {
     switch (env) {
-        case 'production':
-        case 'development':
-            return {
-                networkId: 'default',
-                nodeUrl: 'http://34.94.33.164:3030',
-                masterAccount: 'test.near',
-            };
-        case 'local':
-            return {
-                networkId: 'local',
-                nodeUrl: 'http://localhost:3030',
-                keyPath: `${process.env.HOME}/.near/validator_key.json`,
-            };
-        case 'test':
-            return {
-                networkId: 'local',
-                nodeUrl: 'http://localhost:3030',
-                masterAccount: 'test.near',
-            };
-        case 'test-remote':
-        case 'ci':
-            return {
-                networkId: 'shared-test',
-                nodeUrl: 'http://shared-test.nearprotocol.com:3030',
-                masterAccount: 'test.near',
-            };
-        case 'ci-staging':
-            return {
-                networkId: 'shared-test-staging',
-                nodeUrl: 'http://staging-shared-test.nearprotocol.com:3030',
-                masterAccount: 'test.near',
-            };
-        default:
-            throw Error(`Unconfigured environment '${env}'. Can be configured in test/config.js.`);
+    case 'production':
+    case 'development':
+        return {
+            networkId: 'default',
+            nodeUrl: 'http://34.94.33.164:3030',
+            masterAccount: 'test.near',
+        };
+    case 'local':
+        return {
+            networkId: 'local',
+            nodeUrl: 'http://localhost:3030',
+            keyPath: `${process.env.HOME}/.near/validator_key.json`,
+        };
+    case 'test':
+        return {
+            networkId: 'local',
+            nodeUrl: 'http://localhost:3030',
+            masterAccount: 'test.near',
+        };
+    case 'test-remote':
+    case 'ci':
+        return {
+            networkId: 'shared-test',
+            nodeUrl: 'http://shared-test.nearprotocol.com:3030',
+            masterAccount: 'test.near',
+        };
+    case 'ci-staging':
+        return {
+            networkId: 'shared-test-staging',
+            nodeUrl: 'http://staging-shared-test.nearprotocol.com:3030',
+            masterAccount: 'test.near',
+        };
+    default:
+        throw Error(`Unconfigured environment '${env}'. Can be configured in test/config.js.`);
     }
 };
