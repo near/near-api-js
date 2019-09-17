a6e676ac9fecf5249b903e5e0ff827f0b9d62b47
Karzan Botani <karzan@botani.nu>
2019-09-17 15:30:46 +0200

    Add build files

--- lib/account.d.ts
+++ lib/account.d.ts
@@ -1,4 +1,5 @@
 import BN from 'bn.js';
+import { SignedTransaction } from './transaction';
 import { FinalTransactionResult } from './providers/provider';
 import { Connection } from './connection';
 import { PublicKey } from './utils/key_pair';
@@ -21,13 +22,22 @@ export declare class Account {
     private printLogs;
     private retryTxResult;
     private signAndSendTransaction;
+    private sendTransaction;
+    private signTransaction;
     createAndDeployContract(contractId: string, publicKey: string | PublicKey, data: Uint8Array, amount: BN): Promise<Account>;
+    signCreateAndDeployContract(contractId: string, publicKey: string | PublicKey, data: Uint8Array, amount: BN): Promise<[Uint8Array, SignedTransaction]>;
     sendMoney(receiverId: string, amount: BN): Promise<FinalTransactionResult>;
+    signSendMoney(receiverId: string, amount: BN): Promise<[Uint8Array, SignedTransaction]>;
     createAccount(newAccountId: string, publicKey: string | PublicKey, amount: BN): Promise<FinalTransactionResult>;
+    signCreateAccount(newAccountId: string, publicKey: string | PublicKey, amount: BN): Promise<[Uint8Array, SignedTransaction]>;
     deleteAccount(beneficiaryId: string): Promise<FinalTransactionResult>;
+    signDeleteAccount(beneficiaryId: string): Promise<[Uint8Array, SignedTransaction]>;
     deployContract(data: Uint8Array): Promise<FinalTransactionResult>;
+    signDeployContract(data: Uint8Array): Promise<[Uint8Array, SignedTransaction]>;
     functionCall(contractId: string, methodName: string, args: any, gas: number, amount?: BN): Promise<FinalTransactionResult>;
+    signFunctionCall(contractId: string, methodName: string, args: any, gas: number, amount?: BN): Promise<[Uint8Array, SignedTransaction]>;
     addKey(publicKey: string | PublicKey, contractId?: string, methodName?: string, amount?: BN): Promise<FinalTransactionResult>;
+    signAddKey(publicKey: string | PublicKey, contractId?: string, methodName?: string, amount?: BN): Promise<[Uint8Array, SignedTransaction]>;
     deleteKey(publicKey: string | PublicKey): Promise<FinalTransactionResult>;
     stake(publicKey: string | PublicKey, amount: BN): Promise<FinalTransactionResult>;
     viewFunction(contractId: string, methodName: string, args: any): Promise<any>;
