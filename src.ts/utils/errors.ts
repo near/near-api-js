d454b9f0543fa5524351f3f8e2761b9a775f1e4d
Vladimir Grichina <vgrichina@gmail.com>
2019-12-07 16:17:30 -0800

    yarn fix

--- src.ts/utils/errors.ts
+++ src.ts/utils/errors.ts
@@ -3,4 +3,3 @@ export class PositionalArgsError extends Error {
         super('Contract method calls expect named arguments wrapped in object, e.g. { argName1: argValue1, argName2: argValue2 }');
     }
 }
-
