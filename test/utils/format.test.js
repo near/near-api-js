dc3df65155dc2a4c2e9e62fc2ceba79f6bfc45cb
Vladimir Grichina <vgrichina@gmail.com>
2019-12-10 14:21:25 -0800

    Disable tests explicitly instead of commenting out

--- test/utils/format.test.js
+++ test/utils/format.test.js
@@ -2,32 +2,26 @@
 
 const nearlib = require('../../lib/index');
 
-
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000;
 
-beforeAll(async () => {
-});
-
-beforeEach(async () => {
-});
-
-
-test('formatting attonear amounts', async() => {
-    /*expect(nearlib.utils.format.formatNearAmount('8999999999837087887')).toEqual('0.000008999999999837087887');
+xtest('formatting yoctonear amounts', async() => {
+    expect(nearlib.utils.format.formatNearAmount('8999999999837087887')).toEqual('0.000008999999999837087887');
     expect(nearlib.utils.format.formatNearAmount('8099099999837087887')).toEqual('0.000008099099999837087887');
     expect(nearlib.utils.format.formatNearAmount('8099099999837087887')).not.toEqual('0.000008099099999837087888');
     expect(nearlib.utils.format.formatNearAmount('999998999999999837087887000')).toEqual('999.998999999999837087887');
     expect(nearlib.utils.format.formatNearAmount('1'+'0'.repeat(13))).toEqual('0.00000000001');
-    expect(nearlib.utils.format.formatNearAmount('9999989999999998370878870000000')).toEqual('9,999,989.99999999837087887'); */
+    expect(nearlib.utils.format.formatNearAmount('9999989999999998370878870000000')).toEqual('9,999,989.99999999837087887');
+});
 
+test('formatting attonear amounts', async() => {
     // These tests are in atto near right now. Need to update when the constant changes. 
     expect(nearlib.utils.format.formatNearAmount('1000000000000000000')).toEqual('1');
     expect(nearlib.utils.format.formatNearAmount('10000000999999997410')).toEqual('10.00000099999999741');
     // TODO: do not format smaller values 
 });
 
-/*
-test('converting near to account balance units', async() => {
+// TODO: Renable after moving to yoctonear
+xtest('converting near to account balance units', async() => {
     expect(nearlib.utils.format.parseNearAmount(null)).toEqual(null);
     expect(nearlib.utils.format.parseNearAmount('5.3')).toEqual('5300000000000000000000000');
     expect(nearlib.utils.format.parseNearAmount('5')).toEqual('5000000000000000000000000');
@@ -45,4 +39,4 @@ test('converting near to account balance units', async() => {
     } catch (e) {
         expect(e.toString()).toEqual('Error: Cannot parse \'0.0000080990999998370878871\' as NEAR amount');
     }
-}); */
+});
