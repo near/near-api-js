f1fbf58bc007af033d0a286a0c0196b4ec54e318
Vladimir Grichina <vgrichina@gmail.com>
2020-06-18 19:24:16 -0700

    Restructure browser/Node exports separation
    This helps to avoid missing some common export accidentally

---
+++ src/common-index.ts
@@ -0,0 +1,36 @@
+import * as providers from './providers';
+import * as utils from './utils';
+import * as transactions from './transaction';
+import * as validators from './validators';
+
+import { Account } from './account';
+import * as accountCreator from './account_creator';
+import { Connection } from './connection';
+import { Signer, InMemorySigner } from './signer';
+import { Contract } from './contract';
+import { KeyPair } from './utils/key_pair';
+import { connect, Near } from './near';
+
+// TODO: Deprecate and remove WalletAccount
+import { WalletAccount, WalletConnection } from './wallet-account';
+
+export {
+    accountCreator,
+    providers,
+    utils,
+    transactions,
+    validators,
+
+    Account,
+    Connection,
+    Contract,
+    InMemorySigner,
+    Signer,
+    KeyPair,
+
+    connect,
+    Near,
+
+    WalletAccount,
+    WalletConnection
+};
