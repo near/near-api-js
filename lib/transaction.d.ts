a2135b87573bd7067bee9cf46be17dd8bc7ad61f
Vladimir Grichina <vgrichina@gmail.com>
2020-08-03 16:55:38 -0700

    Add some docs for transaction.functionCall

--- lib/transaction.d.ts
+++ lib/transaction.d.ts
@@ -52,6 +52,15 @@ declare class DeleteAccount extends IAction {
 }
 export declare function createAccount(): Action;
 export declare function deployContract(code: Uint8Array): Action;
+/**
+ * Constructs {@link Action} instance representing contract method call.
+ *
+ * @param methodName the name of the method to call
+ * @param args arguments to pass to method. Can be either plain JS object which gets serialized as JSON automatically
+ *  or `Uint8Array` instance which represents bytes passed as is.
+ * @param gas max amount of gas that method call can use
+ * @param deposit amount of NEAR (in yoctNEAR) to send together with the call
+ */
 export declare function functionCall(methodName: string, args: Uint8Array | object, gas: BN, deposit: BN): Action;
 export declare function transfer(deposit: BN): Action;
 export declare function stake(stake: BN, publicKey: PublicKey): Action;
--- src/transaction.ts
+++ src/transaction.ts
@@ -51,6 +51,15 @@ export function deployContract(code: Uint8Array): Action {
     return new Action({ deployContract: new DeployContract({code}) });
 }
 
+/**
+ * Constructs {@link Action} instance representing contract method call.
+ *
+ * @param methodName the name of the method to call
+ * @param args arguments to pass to method. Can be either plain JS object which gets serialized as JSON automatically
+ *  or `Uint8Array` instance which represents bytes passed as is.
+ * @param gas max amount of gas that method call can use
+ * @param deposit amount of NEAR (in yoctNEAR) to send together with the call
+ */
 export function functionCall(methodName: string, args: Uint8Array | object, gas: BN, deposit: BN): Action {
     const anyArgs = args as any;
     const isUint8Array = anyArgs.byteLength !== undefined && anyArgs.byteLength === anyArgs.length;
