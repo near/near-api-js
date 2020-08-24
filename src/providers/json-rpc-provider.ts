487ddf115203fad0da541f120890aceb6abeae20
Vladimir Grichina <vgrichina@gmail.com>
2020-08-24 15:36:29 -0700

    Add handling for "[-32000] Server error: Timeout error"
    Looks like in some cases timeout errors come in different format.

--- src/providers/json-rpc-provider.ts
+++ src/providers/json-rpc-provider.ts
@@ -167,7 +167,7 @@ export class JsonRpcProvider extends Provider {
                 const errorMessage = `[${response.error.code}] ${response.error.message}: ${response.error.data}`;
                 // NOTE: All this hackery is happening because structured errors not implemented
                 // TODO: Fix when https://github.com/nearprotocol/nearcore/issues/1839 gets resolved
-                if (response.error.data === 'Timeout') {
+                if (response.error.data === 'Timeout' || errorMessage.includes('Timeout error')) {
                     throw new TypedError('send_tx_commit has timed out.', 'TimeoutError');
                 } else {
                     throw new TypedError(errorMessage);
