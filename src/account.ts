cb2de9d3a5fd3a402a46e6ebe241d03c3eff4a43
Matt Lockyer <mattdlockyer@gmail.com>
2020-11-13 09:30:48 -0800

    reorder conditions to catch empty string

--- src/account.ts
+++ src/account.ts
@@ -305,12 +305,15 @@ export class Account {
     /**
      * @param publicKey A public key to be associated with the contract
      * @param contractId NEAR account where the contract is deployed
-     * @param methodNames The method names on the contract that should be allowed to be called. Pass null or '' for no method names and [] for any method names.
+     * @param methodNames The method names on the contract that should be allowed to be called. Pass null for no method names and '' or [] for any method names.
      * @param amount Payment in yoctoⓃ that is sent to the contract during this function call
      * @returns {Promise<FinalExecutionOutcome>}
      * TODO: expand this API to support more options.
      */
     async addKey(publicKey: string | PublicKey, contractId?: string, methodNames?: string|string[], amount?: BN): Promise<FinalExecutionOutcome> {
+        if (methodNames === '') {
+            methodNames = []
+        }
         if (!methodNames) {
             methodNames = ['']
         }
