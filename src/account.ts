e25372e70ba10ba03dfdb4909d6cdab84cb494a0
Josh <thisisjoshford@gmail.com>
2021-02-09 11:16:09 -0800

    update rpc doc link

--- src/account.ts
+++ src/account.ts
@@ -384,7 +384,7 @@ export class Account {
     }
 
     /**
-     * See https://docs.near.org/docs/api/rpc#view-contract-state
+     * See https://docs.near.org/docs/develop/front-end/rpc#view-contract-state
      *
      * Returns the state (key value pairs) of this account's contract based on the key prefix.
      * Pass an empty string for prefix if you would like to return the entire state.
