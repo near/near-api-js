8c9c3bd016f87220c0241fccac443cd2cbd96248
Serhii Volovyk <sergeyvolovyk@gmail.com>
2021-03-21 00:47:52 +0200

    generated error classes deleted

--- lib/utils/errors.d.ts
+++ lib/utils/errors.d.ts
@@ -8,9 +8,11 @@ export declare class TypedError extends Error {
     type: string;
     context?: ErrorContext;
     constructor(message: string, type: string, context?: ErrorContext);
+    isSubtypeOf(errorType: string): boolean;
 }
 export declare class ErrorContext {
     transactionHash?: string;
     errorPath?: Record<string, any>;
-    constructor(transactionHash?: string, errorPath?: Record<string, any>);
+    transactionOutcome?: any;
+    constructor(transactionHash?: string, errorPath?: Record<string, any>, transactionOutcome?: any);
 }
