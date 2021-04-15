27425c886d343a35d29be899f42bfc3d71b2fb54
Ryan Mehta <ryan.mehta@gmail.com>
2021-04-14 18:22:08 -0700

    remove node-fetch added to global in node env

--- src/connect.ts
+++ src/connect.ts
@@ -24,6 +24,9 @@
 import { readKeyFile } from './key_stores/unencrypted_file_system_keystore';
 import { InMemoryKeyStore, MergeKeyStore } from './key_stores';
 import { Near, NearConfig } from './near';
+import fetch from './utils/setup-node-fetch';
+
+global.fetch = fetch;
 
 export interface ConnectConfig extends NearConfig {
     /**
