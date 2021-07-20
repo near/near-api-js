58d527b929191de125320ab7174fc7038ee6ecb8
Serhii Volovyk <sergeyvolovyk@gmail.com>
2021-07-20 18:20:49 +0300

    danger file updated

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,19 +1,15 @@
-import {danger, warn} from 'danger'
+import { schedule, danger } from 'danger';
+import {
+    checkForRelease,
+    checkForNewDependencies,
+    checkForLockfileDiff,
+    checkForTypesInDeps
+} from 'danger-plugin-yarn';
 
-  
-// No PR is too small to include a description of why you made a change
-if (danger.github.pr.body.length < 10) {
-  warn('Please include a description of your PR changes.');
-}
-
-
-// // Request changes to src also include changes to tests.
-// const allFiles = danger.git.modified_files.concat(danger.git.created_files)
-// const hasAppChanges = allFiles.some(p => includes(p, 'src/'))
-// const hasTestChanges = allFiles.some(p => includes(p, '__tests__/'))
-
-// if (hasAppChanges && !hasTestChanges) {
-//   warn('This PR does not include changes to tests, even though it affects app code.');
-// }
-
-  
+schedule(async () => {
+    const packageDiff = await danger.git.JSONDiffForFile("package.json")
+    checkForRelease(packageDiff)
+    checkForNewDependencies(packageDiff)
+    checkForLockfileDiff(packageDiff)
+    checkForTypesInDeps(packageDiff)
+  })
