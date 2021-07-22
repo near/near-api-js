df843094a9df0d2f99ae3ce190b52500a8a87c9a
zurgl <elayar.yacine@gmail.com>
2021-07-22 14:58:08 +0200

    switch to a supported syntax

--- src/near.ts
+++ src/near.ts
@@ -79,7 +79,7 @@ export class Near {
         this.connection = Connection.fromConfig({
             networkId: config.networkId,
             provider: { type: 'JsonRpcProvider', args: { url: config.nodeUrl } },
-            signer: config.signer || { type: 'InMemorySigner', keyStore: config.keyStore || config.deps?.keyStore }
+            signer: config.signer || { type: 'InMemorySigner', keyStore: config.keyStore || config.deps && config.deps.keyStore }
         });
         if (config.masterAccount) {
             // TODO: figure out better way of specifiying initial balance.
