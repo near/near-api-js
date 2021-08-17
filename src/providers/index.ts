d41d4a7f8b77089572096617295e334fb87cd6ae
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-08-17 11:43:13 -0400

    fix: export CodeResult

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,9 +1,10 @@
 /** @hidden @module */
 
-import { Provider, FinalExecutionOutcome, ExecutionOutcomeWithId, getTransactionLastResult, FinalExecutionStatus, FinalExecutionStatusBasic } from './provider';
+import { CodeResult, Provider, FinalExecutionOutcome, ExecutionOutcomeWithId, getTransactionLastResult, FinalExecutionStatus, FinalExecutionStatusBasic } from './provider';
 import { JsonRpcProvider, TypedError, ErrorContext } from './json-rpc-provider';
 
 export {
+    CodeResult,
     Provider,
     FinalExecutionOutcome,
     JsonRpcProvider,
