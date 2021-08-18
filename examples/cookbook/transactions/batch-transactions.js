799c30c7918468bbcf1ed573243c21b161c1bc8a
Serhii Volovyk <sergeyvolovyk@gmail.com>
2021-08-18 17:24:57 +0300

    keyStore error fixed

--- examples/cookbook/transactions/batch-transactions.js
+++ examples/cookbook/transactions/batch-transactions.js
@@ -1,4 +1,4 @@
-const { connect, transactions } = require("near-api-js");
+const { connect, transactions, keyStores } = require("near-api-js");
 const fs = require("fs");
 const path = require("path");
 const homedir = require("os").homedir();
@@ -9,37 +9,35 @@ const WHITELIST_ACCOUNT_ID = "lockup-whitelist.example.testnet";
 const WASM_PATH = "./wasm-files/staking_pool_factory.wasm";
 const TRANSFER_AMOUNT = "50000000000000000000000000";
 
-const config = {
-  keyStore,
-  networkId: "testnet",
-  nodeUrl: "https://rpc.testnet.near.org",
-};
-
 const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
-const keyStore = new nearAPI.keyStores.UnencryptedFileSystemKeyStore(
-  credentialsPath
-);
+const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+
+const config = {
+    keyStore,
+    networkId: "testnet",
+    nodeUrl: "https://rpc.testnet.near.org",
+};
 
 sendTransactions();
 
 async function sendTransactions() {
-  const near = await connect({ ...config, keyStore });
-  const account = await near.account("example.testnet");
-  const newArgs = { staking_pool_whitelist_account_id: WHITELIST_ACCOUNT_ID };
-  const result = await account.signAndSendTransaction({
-    receiverId: CONTRACT_NAME,
-    actions: [
-      transactions.createAccount(),
-      transactions.transfer(TRANSFER_AMOUNT),
-      transactions.deployContract(fs.readFileSync(WASM_PATH)),
-      transactions.functionCall(
-        "new",
-        Buffer.from(JSON.stringify(newArgs)),
-        10000000000000,
-        "0"
-      ),
-    ],
-  });
+    const near = await connect({ ...config, keyStore });
+    const account = await near.account("example.testnet");
+    const newArgs = { staking_pool_whitelist_account_id: WHITELIST_ACCOUNT_ID };
+    const result = await account.signAndSendTransaction({
+        receiverId: CONTRACT_NAME,
+        actions: [
+            transactions.createAccount(),
+            transactions.transfer(TRANSFER_AMOUNT),
+            transactions.deployContract(fs.readFileSync(WASM_PATH)),
+            transactions.functionCall(
+                "new",
+                Buffer.from(JSON.stringify(newArgs)),
+                10000000000000,
+                "0"
+            ),
+        ],
+    });
 
-  console.log(result);
+    console.log(result);
 }
