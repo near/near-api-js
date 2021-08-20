28c8941d2cc182b97728492029d6d719461183ca
Serhii Volovyk <SergeyVolovyk@gmail.com>
2021-08-20 04:43:24 +0300

    batch-transaction fix (#669)
    * keyStore error fixed
    
    * contract path in batch-transaction example fixed, notes added
    
    * remove createAccount and transfer actions as this makes the batch more complicated
    
    Co-authored-by: Mike Purvis <mikedotexe@gmail.com>

--- examples/cookbook/transactions/batch-transactions.js
+++ examples/cookbook/transactions/batch-transactions.js
@@ -1,45 +1,41 @@
-const { connect, transactions } = require("near-api-js");
+const { connect, transactions, keyStores } = require("near-api-js");
 const fs = require("fs");
 const path = require("path");
 const homedir = require("os").homedir();
 
 const CREDENTIALS_DIR = ".near-credentials";
+// NOTE: replace "example" with your accountId
 const CONTRACT_NAME = "contract.example.testnet";
-const WHITELIST_ACCOUNT_ID = "lockup-whitelist.example.testnet";
-const WASM_PATH = "./wasm-files/staking_pool_factory.wasm";
-const TRANSFER_AMOUNT = "50000000000000000000000000";
-
-const config = {
-  keyStore,
-  networkId: "testnet",
-  nodeUrl: "https://rpc.testnet.near.org",
-};
+const WHITELIST_ACCOUNT_ID = "whitelisted-account.example.testnet";
+const WASM_PATH = "../utils/wasm-files/staking_pool_factory.wasm";
 
 const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
-const keyStore = new nearAPI.keyStores.UnencryptedFileSystemKeyStore(
-  credentialsPath
-);
+const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+
+const config = {
+    keyStore,
+    networkId: "testnet",
+    nodeUrl: "https://rpc.testnet.near.org",
+};
 
 sendTransactions();
 
 async function sendTransactions() {
-  const near = await connect({ ...config, keyStore });
-  const account = await near.account("example.testnet");
-  const newArgs = { staking_pool_whitelist_account_id: WHITELIST_ACCOUNT_ID };
-  const result = await account.signAndSendTransaction({
-    receiverId: CONTRACT_NAME,
-    actions: [
-      transactions.createAccount(),
-      transactions.transfer(TRANSFER_AMOUNT),
-      transactions.deployContract(fs.readFileSync(WASM_PATH)),
-      transactions.functionCall(
-        "new",
-        Buffer.from(JSON.stringify(newArgs)),
-        10000000000000,
-        "0"
-      ),
-    ],
-  });
+    const near = await connect({ ...config, keyStore });
+    const account = await near.account(CONTRACT_NAME);
+    const newArgs = { staking_pool_whitelist_account_id: WHITELIST_ACCOUNT_ID };
+    const result = await account.signAndSendTransaction({
+        receiverId: CONTRACT_NAME,
+        actions: [
+            transactions.deployContract(fs.readFileSync(WASM_PATH)),
+            transactions.functionCall(
+                "new",
+                Buffer.from(JSON.stringify(newArgs)),
+                10000000000000,
+                "0"
+            ),
+        ],
+    });
 
-  console.log(result);
+    console.log(result);
 }
