6e74a7372cf329377f71bf9388332e90037ca0d0
Willem Wyndham <willem@cs.umd.edu>
2021-09-28 14:24:33 -0400

    fix: enable logging with env variable

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+process.env.NEAR_PRINT_LOGS = true;
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
--- src/account.ts
+++ src/account.ts
@@ -157,6 +157,7 @@ export class Account {
 
     /** @hidden */
     private printLogsAndFailures(contractId: string, results: [ReceiptLogWithFailure]) {
+      if (process.env["NEAR_PRINT_LOGS"]){
         for (const result of results) {
             console.log(`Receipt${result.receiptIds.length > 1 ? 's' : ''}: ${result.receiptIds.join(', ')}`);
             this.printLogs(contractId, result.logs, '\t');
@@ -164,13 +165,16 @@ export class Account {
                 console.warn(`\tFailure [${contractId}]: ${result.failure}`);
             }
         }
+      }
     }
 
     /** @hidden */
     private printLogs(contractId: string, logs: string[], prefix = '') {
+      if (process.env["NEAR_PRINT_LOGS"]){
         for (const log of logs) {
             console.log(`${prefix}Log [${contractId}]: ${log}`);
         }
+      }
     }
 
     /**
