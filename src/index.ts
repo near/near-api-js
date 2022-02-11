6b65d8063716a5af69bb41d76483dbd34c8f808e
Willem Wyndham <wyndham@cs.unc.edu>
2022-02-11 09:26:10 -0500

    feat: reexport near-units

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * as keyStores from './key_stores/index';
 export * from './common-index';
 export * from './connect';
 export * from './constants';
+export {NEAR, Gas, parse as parseUnits} from 'near-units';
