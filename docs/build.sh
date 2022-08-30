78b2f60f7b3859629f8bdb6f0790af330cda716a
Max <max.khrp@gmail.com>
2022-08-30 15:56:30 +0300

    fix: build docs with separate generator

--- docs/build.sh
+++ docs/build.sh
@@ -3,4 +3,10 @@
 git clone https://github.com/maxhr/near-api-js-docs.git ./builder
 cd builder
 yarn install
+
+export DOCS_ENTRY_POINT=../../packages/near-api-js/src
+export DOCS_TS_CONFIG=../../packages/near-api-js/tsconfig.json
+export DOCS_BASE_PATH=../../packages/near-api-js/src
+export DOCS_README=../README_TYPEDOC.md
+
 yarn build
