be9de2864aa2f42e388d16e700ddfd926eaef745
Max <max.khrp@gmail.com>
2022-08-30 16:17:09 +0300

    fix: build docs with separate generator

--- docs/src/theme/Root.js
+++
@@ -1,8 +0,0 @@
-// https://docusaurus.io/docs/swizzling#wrapper-your-site-with-root
-
-import React from 'react';
-
-// Default implementation, that you can customize
-export default function Root({ children }) {
-    return <>{children}</>;
-}
