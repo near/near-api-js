f7f0598fb2271f284aa20bd43f892cf9669103e8
daoauth <57783762+daoauth@users.noreply.github.com>
2022-09-20 12:17:21 +0900

    fix: add wallet provider

--- packages/near-api-js/src/providers/index.ts
+++ packages/near-api-js/src/providers/index.ts
@@ -2,11 +2,14 @@
 
 import { Provider, FinalExecutionOutcome, ExecutionOutcomeWithId, getTransactionLastResult, FinalExecutionStatus, FinalExecutionStatusBasic } from './provider';
 import { JsonRpcProvider, TypedError, ErrorContext } from './json-rpc-provider';
+import { WalletRpcProvider, WalletProvider } from './wallet-rpc-provider';
 
 export {
     Provider,
+    WalletProvider,
     FinalExecutionOutcome,
     JsonRpcProvider,
+    WalletRpcProvider,
     ExecutionOutcomeWithId,
     FinalExecutionStatus,
     FinalExecutionStatusBasic,
