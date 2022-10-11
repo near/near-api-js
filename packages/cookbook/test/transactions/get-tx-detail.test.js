fe19882f6ada8c62c4fd6609ca09af7b6520b0a7
Andy Haynes <andy@near.org>
2022-10-11 16:48:35 -0700

    test(cookbook): localnet integration for get-tx-detail

--- packages/cookbook/test/transactions/get-tx-detail.test.js
+++ packages/cookbook/test/transactions/get-tx-detail.test.js
@@ -1,18 +1,54 @@
+const { DEFAULT_FUNCTION_CALL_GAS } = require('near-api-js');
+
 const { getTransactions } = require('../../src/transactions/get-tx-detail');
+const { deployGuestbook } = require('../utils');
 
 describe('getTransactions', () => {
+    let account, networkId, nodeUrl;
+
+    beforeAll(async () => {
+        ({
+            account,
+            networkId,
+            nodeUrl,
+        } = await deployGuestbook());
+
+        // wait for next block
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+    });
+
     it('gets transactions for contract between specified blocks', async () => {
-        const contractId = 'relayer.ropsten.testnet';
+        const getBlock = () => account.connection.provider.block({ finality: 'final' });
+        const { header: { hash: startBlock } } = await getBlock();
+
+        const methodName = 'addMessage';
+        for (let i = 0; i < 5; i++) {
+            await account.functionCall({
+                contractId: account.accountId,
+                methodName,
+                args: { text: `iteration-${i}` },
+                gas: DEFAULT_FUNCTION_CALL_GAS,
+                attachedDeposit: '0',
+            });
+        }
+
+        // wait for next block
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+
+        const { header: { hash: endBlock } } = await getBlock();
         const { transactionLinks, transactions } = await getTransactions({
-            accountId: contractId,
-            endBlock: '8aEcKhF7N1Jyw84e6vHW6Hzp3Ep7mSXJ6Rvnsy5qGJPF',
-            networkId: 'testnet',
-            nodeUrl: 'https://archival-rpc.testnet.near.org',
-            startBlock: 'GZ8vKdcgsavkEndkDWHCjuhyqSR2TGnp9VDZbTzd6ufG',
+            accountId: account.accountId,
+            endBlock,
+            networkId,
+            nodeUrl,
+            startBlock,
         });
 
-        expect(transactionLinks.length).toBe(4);
-        expect(transactions.length).toBe(4);
-        transactions.forEach(({ signer_id }) => expect(signer_id).toBe(contractId));
+        expect(transactions.length).toBe(5);
+        transactions.forEach(({ signer_id }) => expect(signer_id).toBe(account.accountId));
+
+        transactionLinks.forEach(({ method }) => {
+            expect(method).toBe(methodName);
+        });
     });
 });
