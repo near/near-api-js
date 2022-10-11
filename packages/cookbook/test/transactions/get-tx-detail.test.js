b37cec69e160e68ef6b992e0bfeba643a1c51d6f
Andy Haynes <andy@near.org>
2022-10-10 17:45:25 -0700

    test(cookbook): testnet integration test

--- packages/cookbook/test/transactions/get-tx-detail.test.js
+++ packages/cookbook/test/transactions/get-tx-detail.test.js
@@ -1,14 +1,18 @@
 const { getTransactions } = require('../../src/transactions/get-tx-detail');
-const { buildTestKeyStore } = require('../utils');
 
 describe('getTransactions', () => {
-    let keyStore;
+    it('gets transactions for contract between specified blocks', async () => {
+        const contractId = 'relayer.ropsten.testnet';
+        const { transactionLinks, transactions } = await getTransactions({
+            accountId: contractId,
+            endBlock: '8aEcKhF7N1Jyw84e6vHW6Hzp3Ep7mSXJ6Rvnsy5qGJPF',
+            networkId: 'testnet',
+            nodeUrl: 'https://archival-rpc.testnet.near.org',
+            startBlock: 'GZ8vKdcgsavkEndkDWHCjuhyqSR2TGnp9VDZbTzd6ufG',
+        });
 
-    beforeAll(async () => {
-        keyStore = await buildTestKeyStore();
-    });
-
-    it('noop', () => {
-        expect(1).toBe(1);
+        expect(transactionLinks.length).toBe(4);
+        expect(transactions.length).toBe(4);
+        transactions.forEach(({ signer_id }) => expect(signer_id).toBe(contractId));
     });
 });
