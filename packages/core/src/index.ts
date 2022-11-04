e32df02f449ef287f4b709fe5d980263c9ebecf8
Andy Haynes <andy@near.org>
2022-11-04 13:32:46 -0700

    refactor: core exports

---
+++ packages/core/src/index.ts
@@ -0,0 +1,3 @@
+export { KeyPair, KeyPairEd25519, KeyType, Signature, PublicKey } from './key_pair';
+export { InMemoryKeyStore, KeyStore } from './key_store';
+export { InMemorySigner, Signer } from './signer';
