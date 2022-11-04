852930eaf268736e8a9111f935c0029c8661814e
Andy Haynes <andy@near.org>
2022-11-04 13:45:02 -0700

    refactor: move key pair code

---
+++ packages/core/src/key_pair/index.ts
@@ -0,0 +1,4 @@
+export { KeyType } from './constants';
+export { KeyPair, Signature } from './key_pair';
+export { KeyPairEd25519 } from './key_pair_ed25519';
+export { PublicKey } from './public_key';
