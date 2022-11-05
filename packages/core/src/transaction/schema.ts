25dfba24358e678f18455e04dc14ee411b9bc80f
Andy Haynes <andy@near.org>
2022-11-04 17:18:37 -0700

    refactor: move transaction code

---
+++ packages/core/src/transaction/schema.ts
@@ -0,0 +1,93 @@
+import { PublicKey } from '../key_pair';
+import {
+    Action,
+    AccessKey,
+    AccessKeyPermission,
+    AddKey,
+    CreateAccount,
+    DeleteAccount,
+    DeleteKey,
+    DeployContract,
+    FullAccessPermission,
+    FunctionCall,
+    FunctionCallPermission,
+    Stake,
+    Transfer,
+} from './actions';
+import { Signature, SignedTransaction, Transaction } from './transaction';
+
+type Class<T = any> = new (...args: any[]) => T;
+
+export const SCHEMA = new Map<Class, any>([
+    [Signature, {kind: 'struct', fields: [
+        ['keyType', 'u8'],
+        ['data', [64]]
+    ]}],
+    [SignedTransaction, {kind: 'struct', fields: [
+        ['transaction', Transaction],
+        ['signature', Signature]
+    ]}],
+    [Transaction, { kind: 'struct', fields: [
+        ['signerId', 'string'],
+        ['publicKey', PublicKey],
+        ['nonce', 'u64'],
+        ['receiverId', 'string'],
+        ['blockHash', [32]],
+        ['actions', [Action]]
+    ]}],
+    [PublicKey, { kind: 'struct', fields: [
+        ['keyType', 'u8'],
+        ['data', [32]]
+    ]}],
+    [AccessKey, { kind: 'struct', fields: [
+        ['nonce', 'u64'],
+        ['permission', AccessKeyPermission],
+    ]}],
+    [AccessKeyPermission, {kind: 'enum', field: 'enum', values: [
+        ['functionCall', FunctionCallPermission],
+        ['fullAccess', FullAccessPermission],
+    ]}],
+    [FunctionCallPermission, {kind: 'struct', fields: [
+        ['allowance', {kind: 'option', type: 'u128'}],
+        ['receiverId', 'string'],
+        ['methodNames', ['string']],
+    ]}],
+    [FullAccessPermission, {kind: 'struct', fields: []}],
+    [Action, {kind: 'enum', field: 'enum', values: [
+        ['createAccount', CreateAccount],
+        ['deployContract', DeployContract],
+        ['functionCall', FunctionCall],
+        ['transfer', Transfer],
+        ['stake', Stake],
+        ['addKey', AddKey],
+        ['deleteKey', DeleteKey],
+        ['deleteAccount', DeleteAccount],
+    ]}],
+    [CreateAccount, { kind: 'struct', fields: [] }],
+    [DeployContract, { kind: 'struct', fields: [
+        ['code', ['u8']]
+    ]}],
+    [FunctionCall, { kind: 'struct', fields: [
+        ['methodName', 'string'],
+        ['args', ['u8']],
+        ['gas', 'u64'],
+        ['deposit', 'u128']
+    ]}],
+    [Transfer, { kind: 'struct', fields: [
+        ['deposit', 'u128']
+    ]}],
+    [Stake, { kind: 'struct', fields: [
+        ['stake', 'u128'],
+        ['publicKey', PublicKey]
+    ]}],
+    [AddKey, { kind: 'struct', fields: [
+        ['publicKey', PublicKey],
+        ['accessKey', AccessKey]
+    ]}],
+    [DeleteKey, { kind: 'struct', fields: [
+        ['publicKey', PublicKey]
+    ]}],
+    [DeleteAccount, { kind: 'struct', fields: [
+        ['beneficiaryId', 'string']
+    ]}],
+]);
---
+++ packages/core/src/transaction/sign.ts
@@ -0,0 +1,40 @@
+import sha256 from 'js-sha256';
+import BN from 'bn.js';
+import { serialize } from 'borsh';
+
+import { Action } from './actions';
+import { SCHEMA } from './schema';
+import { Signer } from '../signer';
+import { createTransaction, Signature, SignedTransaction, Transaction } from './transaction';
+
+/**
+ * Signs a given transaction from an account with given keys, applied to the given network
+ * @param transaction The Transaction object to sign
+ * @param signer The {Signer} object that assists with signing keys
+ * @param accountId The human-readable NEAR account name
+ * @param networkId The targeted network. (ex. default, betanet, etc…)
+ */
+async function signTransactionObject(transaction: Transaction, signer: Signer, accountId?: string, networkId?: string): Promise<[Uint8Array, SignedTransaction]> {
+    const message = serialize(SCHEMA, transaction);
+    const hash = new Uint8Array(sha256.sha256.array(message));
+    const signature = await signer.signMessage(message, accountId, networkId);
+    const signedTx = new SignedTransaction({
+        transaction,
+        signature: new Signature({ keyType: transaction.publicKey.keyType, data: signature.signature })
+    });
+    return [hash, signedTx];
+}
+
+export async function signTransaction(transaction: Transaction, signer: Signer, accountId?: string, networkId?: string): Promise<[Uint8Array, SignedTransaction]>;
+export async function signTransaction(receiverId: string, nonce: BN, actions: Action[], blockHash: Uint8Array, signer: Signer, accountId?: string, networkId?: string): Promise<[Uint8Array, SignedTransaction]>;
+export async function signTransaction(...args): Promise<[Uint8Array, SignedTransaction]> {
+    if (args[0].constructor === Transaction) {
+        const [ transaction, signer, accountId, networkId ] = args;
+        return signTransactionObject(transaction, signer, accountId, networkId);
+    } else {
+        const [ receiverId, nonce, actions, blockHash, signer, accountId, networkId ] = args;
+        const publicKey = await signer.getPublicKey(accountId, networkId);
+        const transaction = createTransaction(accountId, publicKey, receiverId, nonce, actions, blockHash);
+        return signTransactionObject(transaction, signer, accountId, networkId);
+    }
+}
