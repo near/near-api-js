302fc3b40eba1e76ee1be89442af9acbc2dda4ca
Andy Haynes <andy@near.org>
2022-11-14 15:36:56 -0800

    refactor: move error modules to core

---
+++ packages/core/src/errors/index.ts
@@ -0,0 +1,14 @@
+export {
+    ArgumentTypeError,
+    ErrorContext,
+    PositionalArgsError,
+    TypedError,
+    logWarning,
+} from './errors';
+export {
+    ServerError,
+    formatError,
+    getErrorTypeFromErrorMessage,
+    parseResultError,
+    parseRpcError,
+} from './rpc_errors';
