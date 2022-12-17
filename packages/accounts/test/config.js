72d24adf449fd172d235951b4290e8c5045f6172
Andy Haynes <andy@near.org>
2022-12-16 16:16:53 -0800

    refactor: moving tests

---
+++ packages/accounts/test/config.js
@@ -0,0 +1,44 @@
+module.exports = function getConfig(env) {
+    switch (env) {
+    case 'production':
+    case 'mainnet':
+        return {
+            networkId: 'mainnet',
+            nodeUrl: 'https://rpc.mainnet.near.org',
+            walletUrl: 'https://wallet.near.org',
+            helperUrl: 'https://helper.mainnet.near.org',
+        };
+    case 'development':
+    case 'testnet':
+        return {
+            networkId: 'default',
+            nodeUrl: 'https://rpc.testnet.near.org',
+            walletUrl: 'https://wallet.testnet.near.org',
+            helperUrl: 'https://helper.testnet.near.org',
+            masterAccount: 'test.near',
+        };
+    case 'betanet':
+        return {
+            networkId: 'betanet',
+            nodeUrl: 'https://rpc.betanet.near.org',
+            walletUrl: 'https://wallet.betanet.near.org',
+            helperUrl: 'https://helper.betanet.near.org',
+        };
+    case 'local':
+        return {
+            networkId: 'local',
+            nodeUrl: 'http://localhost:3030',
+            keyPath: `${process.env.HOME}/.near/validator_key.json`,
+            walletUrl: 'http://localhost:4000/wallet',
+        };
+    case 'test':
+    case 'ci':
+        return {
+            networkId: 'shared-test',
+            nodeUrl: 'https://rpc.ci-testnet.near.org',
+            masterAccount: 'test.near',
+        };
+    default:
+        throw Error(`Unconfigured environment '${env}'. Can be configured in src/config.js.`);
+    }
+};
