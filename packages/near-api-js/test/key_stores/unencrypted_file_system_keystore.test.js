791c5f71c48f8b2e1ba344a88010c692381674d8
Andy Haynes <andy@near.org>
2023-01-03 11:49:58 -0800

    refactor: inline test method

--- packages/near-api-js/test/key_stores/unencrypted_file_system_keystore.test.js
+++ packages/near-api-js/test/key_stores/unencrypted_file_system_keystore.test.js
@@ -4,8 +4,7 @@ const rimraf  = require('util').promisify(require('rimraf'));
 const nearApi = require('../../src/index');
 const UnencryptedFileSystemKeyStore = nearApi.keyStores.UnencryptedFileSystemKeyStore;
 const KeyPair = nearApi.utils.KeyPairEd25519;
-const { ensureDir } = require('../test-utils');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const KEYSTORE_PATH = '../test-keys';
@@ -15,7 +14,11 @@ describe('Unencrypted file system keystore', () => {
 
     beforeAll(async () => {
         await rimraf(KEYSTORE_PATH);
-        await ensureDir(KEYSTORE_PATH);
+        try {
+            await fs.mkdir(KEYSTORE_PATH, { recursive: true });
+        } catch (err) {
+            if (err.code !== 'EEXIST') throw err;
+        }
         ctx.keyStore = new UnencryptedFileSystemKeyStore(KEYSTORE_PATH);
     });
 
@@ -29,7 +32,7 @@ describe('Unencrypted file system keystore', () => {
         const key1 = KeyPair.fromRandom();
         await ctx.keyStore.setKey('network', 'account', key1);
         const keyFilePath = ctx.keyStore.getKeyFilePath('network', 'account');
-        const content = fs.readFileSync(keyFilePath);
+        const content = await fs.readFile(keyFilePath);
         const accountInfo = JSON.parse(content.toString());
         expect(accountInfo.public_key).toEqual(key1.getPublicKey().toString());
     });
