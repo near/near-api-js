8898fea38e6893315627a9eb045223349836c3c0
Andy Haynes <andy@near.org>
2023-03-03 17:21:48 -0800

    feat: move Signature to separate module

--- packages/transactions/src/index.ts
+++ packages/transactions/src/index.ts
@@ -3,4 +3,5 @@ export * from './actions';
 export * from './create_transaction';
 export * from './serialization';
 export * from './sign';
+export * from './signature';
 export * from './transaction';
