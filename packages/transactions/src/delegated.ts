690e7fc80502ffde75cc894ebda5e7a36b0185a1
Andy Haynes <andy@near.org>
2023-03-20 14:40:01 -0700

    feat: method for building delegate actions

--- packages/transactions/src/delegated.ts
+++ packages/transactions/src/delegated.ts
@@ -31,3 +31,30 @@ export class DelegateAction extends Assignable {
     maxBlockHeight: number;
     publicKey: PublicKey;
 }
+
+/**
+ * Compose a delegate action for inclusion with a meta transaction signed on the sender's behalf
+ * @params.actions The set of actions to be included in the meta transaction
+ * @params.maxBlockHeight The maximum block height for which this action can be executed as part of a transaction
+ * @params.nonce Current nonce on the access key used to sign the delegate action
+ * @params.publicKey Public key for the access key used to sign the delegate action
+ * @params.receiverId Account ID for the intended receiver of the meta transaction
+ * @params.senderId Account ID for the intended signer of the delegate action
+ */
+export function buildDelegateAction({
+    actions,
+    maxBlockHeight,
+    nonce,
+    publicKey,
+    receiverId,
+    senderId,
+}: DelegateAction): DelegateAction {
+    return new DelegateAction({
+        senderId,
+        receiverId,
+        actions,
+        nonce,
+        maxBlockHeight,
+        publicKey,
+    });
+}
