24830e1c436022d1b04f67d094ebd64343f2bf3d
Andy Haynes <andy@near.org>
2023-03-20 14:40:47 -0700

    feat: module for building meta transactions

---
+++ packages/transactions/src/meta_transaction.ts
@@ -0,0 +1,44 @@
+import { PublicKey } from '@near-js/crypto';
+import BN from 'bn.js';
+import { baseDecode } from 'borsh';
+
+import { actionCreators } from './action_creators';
+import { SignedDelegate } from './actions';
+import { Transaction } from './transaction';
+
+const { signedDelegate: signedDelegateAction } = actionCreators;
+
+interface MetaTransactionOptions {
+    accountId: string;
+    blockHash: string;
+    signedDelegate: SignedDelegate;
+    signerNonce: BN;
+    signerPublicKey: PublicKey;
+}
+
+/**
+ * Compose a meta transaction encapsulating a signed delegate action
+ * @params.accountId Account ID for the sender of the transaction, i.e. the meta transaction relayer account
+ * @params.blockHash Current block hash
+ * @params.signedDelegate SignedDelegate action instance
+ * @params.signerNonce Current nonce on the access key used to sign the meta transaction
+ * @params.signerPublicKey Public key for the access key used to sign the meta transaction
+ */
+export function buildMetaTransaction({
+    accountId,
+    blockHash,
+    signedDelegate,
+    signerNonce,
+    signerPublicKey,
+}: MetaTransactionOptions): Transaction {
+    const { delegateAction } = signedDelegate;
+
+    return new Transaction({
+        signerId: accountId,
+        publicKey: signerPublicKey,
+        nonce: signerNonce,
+        receiverId: delegateAction.senderId,
+        actions: [signedDelegateAction(signedDelegate)],
+        blockHash: baseDecode(blockHash),
+    });
+}
