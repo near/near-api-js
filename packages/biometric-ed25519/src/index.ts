d6f12d02c9e4b29d2c0ce16ba91a01435ed6648b
Daryl Collins <daryl@darylcollins.com>
2023-04-28 15:17:29 +0100

    fix: remove username length check, format code

--- packages/biometric-ed25519/src/index.ts
+++ packages/biometric-ed25519/src/index.ts
@@ -1,13 +1,20 @@
 import base64 from "@hexagon/base64";
-import { eddsa as EDDSA } from "elliptic";
-import { Sha256 } from "@aws-crypto/sha256-js";
-import { Buffer } from "buffer";
+import {eddsa as EDDSA} from "elliptic";
+import {Sha256} from "@aws-crypto/sha256-js";
+import {Buffer} from "buffer";
 import asn1 from "asn1-parser";
-import { KeyPair } from "@near-js/crypto";
-import { baseEncode } from "borsh";
-import { getCleanName, preformatMakeCredReq, get64BytePublicKeyFromPEM, preformatGetAssertReq, publicKeyCredentialToJSON, recoverPublicKey } from "./utils";
-import { Fido2 } from "./fido2";
-import { AssertionResponse } from "./index.d";
+import {KeyPair} from "@near-js/crypto";
+import {baseEncode} from "borsh";
+import {
+    validateUsername,
+    preformatMakeCredReq,
+    get64BytePublicKeyFromPEM,
+    preformatGetAssertReq,
+    publicKeyCredentialToJSON,
+    recoverPublicKey
+} from "./utils";
+import {Fido2} from "./fido2";
+import {AssertionResponse} from "./index.d";
 
 const CHALLENGE_TIMEOUT_MS = 90 * 1000;
 const RP_NAME = "NEAR_API_JS_WEBAUTHN";
@@ -15,89 +22,90 @@ const RP_ID = location.hostname;
 
 const f2l: Fido2 = new Fido2();
 const init: () => Promise<void> = async () => {
-  await f2l.init({
-    rpId: RP_ID,
-    rpName: RP_NAME,
-    timeout: CHALLENGE_TIMEOUT_MS,
-  });
+    await f2l.init({
+        rpId: RP_ID,
+        rpName: RP_NAME,
+        timeout: CHALLENGE_TIMEOUT_MS,
+    });
 };
 
 if (window && !window.Buffer) {
-  window.Buffer = Buffer;
+    window.Buffer = Buffer;
 }
 
-export const createKey = async (username: string): Promise<KeyPair>  => {
-  const cleanUserName = getCleanName(username);
-  if (!f2l.f2l) {
-    await init();
-  }
+export const createKey = async (username: string): Promise<KeyPair> => {
+    const cleanUserName = validateUsername(username);
+    if (!f2l.f2l) {
+        await init();
+    }
 
-  const id = base64.fromString(cleanUserName, true);
-  const challengeMakeCred = await f2l.registration({
-    username: cleanUserName,
-    displayName: cleanUserName,
-    id,
-  });
-  const publicKey = preformatMakeCredReq(challengeMakeCred);
+    const id = base64.fromString(cleanUserName, true);
+    const challengeMakeCred = await f2l.registration({
+        username: cleanUserName,
+        displayName: cleanUserName,
+        id,
+    });
+    const publicKey = preformatMakeCredReq(challengeMakeCred);
 
-  return navigator.credentials.create({ publicKey })
-    .then(async (res) => {
-      const result = await f2l.attestation({
-        clientAttestationResponse: res,
-        origin,
-        challenge: challengeMakeCred.challenge
-      });
-      const publicKey = result.authnrData.get("credentialPublicKeyPem");
-      const publicKeyBytes = get64BytePublicKeyFromPEM(publicKey);
-      const ed = new EDDSA("ed25519");
-      const edSha256 = new Sha256();
-      edSha256.update(Buffer.from(publicKeyBytes));
-      const key = ed.keyFromSecret(await edSha256.digest());
-      return KeyPair.fromString(baseEncode(new Uint8Array(Buffer.concat([key.getSecret(), Buffer.from(key.getPublic())]))));
-    })
+    return navigator.credentials.create({publicKey})
+        .then(async (res) => {
+            const result = await f2l.attestation({
+                clientAttestationResponse: res,
+                origin,
+                challenge: challengeMakeCred.challenge
+            });
+            const publicKey = result.authnrData.get("credentialPublicKeyPem");
+            const publicKeyBytes = get64BytePublicKeyFromPEM(publicKey);
+            const ed = new EDDSA("ed25519");
+            const edSha256 = new Sha256();
+            edSha256.update(Buffer.from(publicKeyBytes));
+            const key = ed.keyFromSecret(await edSha256.digest());
+            return KeyPair.fromString(baseEncode(new Uint8Array(Buffer.concat([key.getSecret(), Buffer.from(key.getPublic())]))));
+        })
 };
 
 // Ecrecover returns two possible public keys for a given signature
 export const getKeys = async (username: string): Promise<[KeyPair, KeyPair]> => {
-  const cleanUserName = getCleanName(username);
-  if (!f2l.f2l) {
-    await init();
-  }
-  const assertionOptions = await f2l.login();
-  const options = {
-    ...assertionOptions,
-    username: cleanUserName,
-    allowCredentials: [],
-  };
-  const publicKey = preformatGetAssertReq(options);
+    const cleanUserName = validateUsername(username);
+    if (!f2l.f2l) {
+        await init();
+    }
+    const assertionOptions = await f2l.login();
+    const options = {
+        ...assertionOptions,
+        username: cleanUserName,
+        allowCredentials: [],
+    };
+    const publicKey = preformatGetAssertReq(options);
 
-  return navigator.credentials.get({ publicKey })
-    .then(async (response: Credential)=> {
-      const getAssertionResponse: AssertionResponse = publicKeyCredentialToJSON(response);
-      const signature = base64.toArrayBuffer(getAssertionResponse.response.signature, true);
+    return navigator.credentials.get({publicKey})
+        .then(async (response: Credential) => {
+            const getAssertionResponse: AssertionResponse = publicKeyCredentialToJSON(response);
+            const signature = base64.toArrayBuffer(getAssertionResponse.response.signature, true);
 
-      //@ts-ignore
-      const parser = asn1?.ASN1?.parse || window?.ASN1?.parse;
-      const rAndS = parser(new Uint8Array(signature));
-      const clientDataSha256 = new Sha256();
-      clientDataSha256.update(
-        Buffer.from(new Uint8Array(base64.toArrayBuffer(getAssertionResponse.response.clientDataJSON, true)))
-      );
-      const clientDataJSONHash = await clientDataSha256.digest();
-      const AuthenticatiorDataJSONHash = Buffer.from(new Uint8Array(base64.toArrayBuffer(getAssertionResponse.response.authenticatorData, true)))
-      const authenticatorAndClientDataJSONHash = Buffer.concat([AuthenticatiorDataJSONHash, clientDataJSONHash]);
-    
-      const correctPKs = await recoverPublicKey(rAndS.children[0].value, rAndS.children[1].value, authenticatorAndClientDataJSONHash, 0);
-      const ed = new EDDSA("ed25519");
-      const firstEdSha256 = new Sha256();
-      firstEdSha256.update(Buffer.from(correctPKs[0]));
-      const secondEdSha256 = new Sha256();
-      secondEdSha256.update(Buffer.from(correctPKs[1]));
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            //@ts-ignore
+            const parser = asn1?.ASN1?.parse || window?.ASN1?.parse;
+            const rAndS = parser(new Uint8Array(signature));
+            const clientDataSha256 = new Sha256();
+            clientDataSha256.update(
+                Buffer.from(new Uint8Array(base64.toArrayBuffer(getAssertionResponse.response.clientDataJSON, true)))
+            );
+            const clientDataJSONHash = await clientDataSha256.digest();
+            const AuthenticatiorDataJSONHash = Buffer.from(new Uint8Array(base64.toArrayBuffer(getAssertionResponse.response.authenticatorData, true)))
+            const authenticatorAndClientDataJSONHash = Buffer.concat([AuthenticatiorDataJSONHash, clientDataJSONHash]);
 
-      const firstED = ed.keyFromSecret(await firstEdSha256.digest());
-      const secondED = ed.keyFromSecret(await secondEdSha256.digest());
-      const firstKeyPair = KeyPair.fromString(baseEncode(new Uint8Array(Buffer.concat([firstED.getSecret(), Buffer.from(firstED.getPublic())]))));
-      const secondKeyPair = KeyPair.fromString(baseEncode(new Uint8Array(Buffer.concat([secondED.getSecret(), Buffer.from(secondED.getPublic())]))));
-      return [firstKeyPair, secondKeyPair];
-    });
+            const correctPKs = await recoverPublicKey(rAndS.children[0].value, rAndS.children[1].value, authenticatorAndClientDataJSONHash, 0);
+            const ed = new EDDSA("ed25519");
+            const firstEdSha256 = new Sha256();
+            firstEdSha256.update(Buffer.from(correctPKs[0]));
+            const secondEdSha256 = new Sha256();
+            secondEdSha256.update(Buffer.from(correctPKs[1]));
+
+            const firstED = ed.keyFromSecret(await firstEdSha256.digest());
+            const secondED = ed.keyFromSecret(await secondEdSha256.digest());
+            const firstKeyPair = KeyPair.fromString(baseEncode(new Uint8Array(Buffer.concat([firstED.getSecret(), Buffer.from(firstED.getPublic())]))));
+            const secondKeyPair = KeyPair.fromString(baseEncode(new Uint8Array(Buffer.concat([secondED.getSecret(), Buffer.from(secondED.getPublic())]))));
+            return [firstKeyPair, secondKeyPair];
+        });
 };
--- packages/biometric-ed25519/src/utils.ts
+++ packages/biometric-ed25519/src/utils.ts
@@ -1,108 +1,94 @@
 import base64 from "@hexagon/base64";
-import { ec as EC } from "elliptic";
-import { Sha256 } from "@aws-crypto/sha256-js";
-import { PublicKey } from "@near-js/crypto";
+import {ec as EC} from "elliptic";
+import {Sha256} from "@aws-crypto/sha256-js";
+import {PublicKey} from "@near-js/crypto";
 
 const USER_NAME_MAX_LENGTH = 25;
 
-export const cleanName = (name: string): string => {
-  try {
-    return name.toString();
-  } catch (e) {
-    throw Error('Invalid username');
-  }
-};
-
 export const preformatMakeCredReq = (makeCredReq) => {
-  const challenge = base64.toArrayBuffer(makeCredReq.challenge,true);
-  const userId = base64.toArrayBuffer(makeCredReq.user.id,true);
+    const challenge = base64.toArrayBuffer(makeCredReq.challenge, true);
+    const userId = base64.toArrayBuffer(makeCredReq.user.id, true);
 
-  return {
-    ...makeCredReq,
-    challenge,
-    user: {
-        ...makeCredReq.user,
-        id: userId,
-    },
-    ...(makeCredReq.excludeCredentials ? { excludeCredentials:  makeCredReq.excludeCredentials.map((e) => { return { id: base64.toArrayBuffer(e.id, true), type: e.type }})} : {})
-  }
+    return {
+        ...makeCredReq,
+        challenge,
+        user: {
+            ...makeCredReq.user,
+            id: userId,
+        },
+        ...(makeCredReq.excludeCredentials ? {
+            excludeCredentials: makeCredReq.excludeCredentials.map((e) => {
+                return {id: base64.toArrayBuffer(e.id, true), type: e.type}
+            })
+        } : {})
+    }
 };
 
 export const get64BytePublicKeyFromPEM = (publicKey: PublicKey) => {
-  var prefix = '\n';
-  const publicKeyBase64 = publicKey.toString().split(prefix)
-  return base64.toArrayBuffer(`${publicKeyBase64[1]}${publicKeyBase64[2]}`).slice(27);
+    const prefix = '\n';
+    const publicKeyBase64 = publicKey.toString().split(prefix)
+    return base64.toArrayBuffer(`${publicKeyBase64[1]}${publicKeyBase64[2]}`).slice(27);
 };
 
-export const getCleanName = (name: string): string => {
-  if (!name) {
-    throw new Error("username is required");
-  }
-  const cleanUserName = cleanName(name);
-  if (!cleanUserName || typeof cleanUserName !== 'string') {
-    throw new Error("Invalid username");
-  };
-  if (typeof cleanUserName === 'string' && cleanUserName.length > USER_NAME_MAX_LENGTH) {
-    throw new Error("username should be less than 25 characters");
-  }
-  return cleanUserName;
+export const validateUsername = (name: string): string => {
+    if (!name) {
+        throw new Error("username is required");
+    }
+    return name;
 };
 
 export const preformatGetAssertReq = (getAssert) => {
-  getAssert.challenge = base64.toArrayBuffer(getAssert.challenge,true);
-    
-	// Allow any credential, this will be handled later
-	for(let allowCred of getAssert.allowCredentials) {
-		allowCred.id = base64.toArrayBuffer(allowCred.id,true);
-	}
+    getAssert.challenge = base64.toArrayBuffer(getAssert.challenge, true);
 
-	return getAssert;
+    // Allow any credential, this will be handled later
+    for (const allowCred of getAssert.allowCredentials) {
+        allowCred.id = base64.toArrayBuffer(allowCred.id, true);
+    }
+
+    return getAssert;
 };
 
 
 export const publicKeyCredentialToJSON = (pubKeyCred) => {
-  if(pubKeyCred instanceof Array) {
-      let arr = [];
-      for(let i of pubKeyCred)
-          arr.push(publicKeyCredentialToJSON(i));
+    if (pubKeyCred instanceof Array) {
+        const arr = [];
+        for (const i of pubKeyCred)
+            arr.push(publicKeyCredentialToJSON(i));
 
-      return arr;
-  }
+        return arr;
+    }
 
-  if(pubKeyCred instanceof ArrayBuffer) {
-      return base64.fromArrayBuffer(pubKeyCred,true);
-  }
+    if (pubKeyCred instanceof ArrayBuffer) {
+        return base64.fromArrayBuffer(pubKeyCred, true);
+    }
 
-  if(pubKeyCred instanceof Object) {
-      let obj = {};
+    if (pubKeyCred instanceof Object) {
+        const obj = {};
 
-      for (let key in pubKeyCred) {
-          obj[key] = publicKeyCredentialToJSON(pubKeyCred[key]);
-      }
+        for (const key in pubKeyCred) {
+            obj[key] = publicKeyCredentialToJSON(pubKeyCred[key]);
+        }
 
-      return obj;
-  }
+        return obj;
+    }
 
-  return pubKeyCred;
+    return pubKeyCred;
 };
 
 export const recoverPublicKey = async (r, s, message, recovery) => {
-  const ec = new EC("p256");
-  const sigObj = { r, s };
+    const ec = new EC("p256");
+    const sigObj = {r, s};
 
-  if (recovery !== 0 && recovery !== 1) {
-      throw new Error('Invalid recovery parameter');
-  }
-  const hash = new Sha256();
-  hash.update(message);
-  const h = await hash.digest();
-  let Q, P;
-  try {
-      Q = ec.recoverPubKey(h, sigObj, 0);
-      P = ec.recoverPubKey(h, sigObj, 1);
-  } catch (err) {
-      throw err;
-  }
-  let publicKeys = [Buffer.from(new Uint8Array(Buffer.from(Q.encode(true, false))).subarray(1, 65)), Buffer.from(new Uint8Array(Buffer.from(P.encode(true, false))).subarray(1, 65))]
-  return publicKeys;
+    if (recovery !== 0 && recovery !== 1) {
+        throw new Error('Invalid recovery parameter');
+    }
+    const hash = new Sha256();
+    hash.update(message);
+    const h = await hash.digest();
+    const Q = ec.recoverPubKey(h, sigObj, 0)
+    const P = ec.recoverPubKey(h, sigObj, 1);
+    return [
+        Buffer.from(new Uint8Array(Buffer.from(Q.encode(true, false))).subarray(1, 65)),
+        Buffer.from(new Uint8Array(Buffer.from(P.encode(true, false))).subarray(1, 65))
+    ];
 };
