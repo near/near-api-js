c97077f1a5280335b94840f05f0c47de2ec9a0b0
Shaun Hamilton <shauhami020@gmail.com>
2023-10-30 09:52:05 +0000

    fix: add check for process global to exist
    To prevent error 'process undefined' in environments where global process object does not exist

--- packages/accounts/src/account.ts
+++ packages/accounts/src/account.ts
@@ -363,7 +363,7 @@ export class Account {
      * @param beneficiaryId The NEAR account that will receive the remaining Ⓝ balance from the account being deleted
      */
     async deleteAccount(beneficiaryId: string) {
-        if (!process.env['NEAR_NO_LOGS']) {
+        if (!(typeof process === 'object' && process.env['NEAR_NO_LOGS'])) {
             console.log('Deleting an account does not automatically transfer NFTs and FTs to the beneficiary address. Ensure to transfer assets before deleting.');
         }
         return this.signAndSendTransaction({
