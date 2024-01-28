219d6c043ad3d7fd154cd172b47b596b4c268c4a
Den <mirloxd3@gmail.com>
2024-01-28 17:23:06 +0200

    Revert "fix: sync up error schema"
    This reverts commit 58bc887111ebe9afda1d57727074f48aedf981fa.

--- packages/utils/fetch_error_schema.js
+++ packages/utils/fetch_error_schema.js
@@ -2,8 +2,8 @@ const https = require('https');
 const fs = require('fs');
 
 const ERROR_SCHEMA_URL =
-    'https://raw.githubusercontent.com/near/nearcore/master/chain/jsonrpc/res/rpc_errors_schema.json';
-const TARGET_SCHEMA_FILE_PATH = `${process.argv[2] ||  process.cwd()}/src/errors/rpc_error_schema.json`;
+    'https://raw.githubusercontent.com/nearprotocol/nearcore/4c1149974ccf899dbcb2253a3e27cbab86dc47be/chain/jsonrpc/res/rpc_errors_schema.json';
+const TARGET_SCHEMA_FILE_PATH = `${process.argv[2] ||  process.cwd()}/src/errors/rpc_error_schema.json'`;
 
 https
     .get(ERROR_SCHEMA_URL, resp => {
