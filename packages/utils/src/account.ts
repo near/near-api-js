b3faca327ee34f18350a7073edf096aa855afee9
Andy Haynes <andy@near.org>
2024-05-29 16:25:00 -0700

    style: apply lint rule to other packages

---
+++ packages/utils/src/account.ts
@@ -0,0 +1,32 @@
+import type { AccountView, NearProtocolConfig } from '@near-js/types';
+
+interface AccountBalanceDetail {
+    available: BigInt;
+    staked: BigInt;
+    stateStaked: BigInt;
+    total: BigInt;
+}
+
+/**
+ * Parse account balance details
+ *
+ * @param account account state from `view_account`
+ * @param protocolConfig
+ * @returns AccountBalanceDetail
+ */
+export function parseBalance(account: AccountView, protocolConfig: NearProtocolConfig): AccountBalanceDetail {
+    const { amount, locked, storage_usage } = account;
+
+    const costPerByte = BigInt(protocolConfig.runtime_config.storage_amount_per_byte);
+    const stateStaked = BigInt(storage_usage) * costPerByte;
+    const staked = BigInt(locked);
+    const total = BigInt(amount) + staked;
+    const available = total - (staked > stateStaked ? staked : stateStaked);
+
+    return {
+        available,
+        staked,
+        stateStaked,
+        total,
+    };
+}
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,4 @@
+export * from './account';
 export * from './constants';
 export * from './errors';
 export * from './format';
