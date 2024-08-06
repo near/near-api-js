507701057fd5c92c0a3570f85a266bb9b6a64fd9
Andy Haynes <andy@near.org>
2024-08-06 11:21:40 -0700

    refactor: enum class

--- packages/near-api-js/src/utils/enums.ts
+++ packages/near-api-js/src/utils/enums.ts
@@ -1,14 +1,1 @@
-/** @hidden @module */
-export abstract class Enum {
-    enum: string;
-
-    constructor(properties: any) {
-        if (Object.keys(properties).length !== 1) {
-            throw new Error('Enum can only take single value');
-        }
-        Object.keys(properties).map((key: string) => {
-            (this as any)[key] = properties[key];
-            this.enum = key;
-        });
-    }
-}
+export { Enum } from '@near-js/types';
