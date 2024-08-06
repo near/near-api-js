7bb21610fd8f6c6ac988b61b21ca6863b5c12f9e
Andy Haynes <andy@near.org>
2024-08-06 12:01:13 -0700

    feat: borsh enum classes must extend Enum

--- packages/transactions/src/signature.ts
+++ packages/transactions/src/signature.ts
@@ -1,18 +1,33 @@
 import { KeyType } from '@near-js/crypto';
+import { Enum } from '@near-js/types';
 
 class ED25519Signature { keyType: KeyType = KeyType.ED25519; data: Uint8Array; }
 class SECP256K1Signature { keyType: KeyType = KeyType.SECP256K1; data: Uint8Array; }
 
-export class Signature {
+function resolveEnumKeyName(keyType: KeyType) {
+    switch (keyType) {
+        case KeyType.ED25519: {
+            return 'ed25519Signature';
+        }
+        case KeyType.SECP256K1: {
+            return 'secp256k1Signature'
+        }
+        default: {
+            throw Error(`unknown type ${keyType}`);
+        }
+    }
+}
+
+export class Signature extends Enum {
+    enum: string;
     ed25519Signature?: ED25519Signature;
     secp256k1Signature?: SECP256K1Signature;
 
-    constructor({ keyType, data }: { keyType: KeyType, data: Uint8Array }) {
-        if (keyType === KeyType.ED25519) {
-            this.ed25519Signature = { keyType, data };
-        } else if (keyType === KeyType.SECP256K1) {
-            this.secp256k1Signature = { keyType, data };
-        }
+    constructor(signature: { keyType: KeyType, data: Uint8Array }) {
+        const keyName = resolveEnumKeyName(signature.keyType);
+        super({ [keyName]: signature });
+        this[keyName] = signature;
+        this.enum = keyName;
     }
 
     get signature() {
