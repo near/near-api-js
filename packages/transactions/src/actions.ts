76dac8e784c44626dfb316cdf30b9f24d62585f9
Andy Haynes <andy@near.org>
2024-08-06 11:21:40 -0700

    refactor: enum class

--- packages/transactions/src/actions.ts
+++ packages/transactions/src/actions.ts
@@ -1,22 +1,8 @@
 import { PublicKey } from '@near-js/crypto';
+import { Enum } from '@near-js/types';
 
 import { DelegateAction } from './delegate';
 import { Signature } from './signature';
-
-// TODO determine why subclassing is still necessary even though `enum`
-//  cannot be set in the base class or it will not be borsh-serializable
-abstract class Enum {
-    abstract enum: string;
-
-    constructor(properties: any) {
-        if (Object.keys(properties).length !== 1) {
-            throw new Error('Enum can only take single value');
-        }
-        Object.keys(properties).map((key: string) => {
-            (this as any)[key] = properties[key];
-        });
-    }
-}
 
 export class FunctionCallPermission {
     allowance?: bigint;
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,2 +1,3 @@
+export * from './enum';
 export * from './errors';
 export * from './provider';
