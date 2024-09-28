71321d48ea172bdaa5e482c4070f11a3ed7ffa5e
Andy Haynes <andy@near.org>
2024-09-27 18:06:42 -0700

    chore: cleanup unused constants

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -1,11 +1,4 @@
-export const ACCOUNT_CREATION_DEPOSIT = BigInt(100);
-export const MAINNET_RPC_URL = 'https://rpc.mainnet.near.org';
-export const TESTNET_RPC_URL = 'https://rpc.testnet.near.org';
-export const DEFAULT_FILESYSTEM_KEYSTORE_PATH = '.near-credentials';
-export const DEFAULT_META_TRANSACTION_BLOCK_HEIGHT_TTL = BigInt(100);
-
-export const LEDGER_HD_PATH = `44'/397'/0'/0'/1`;
-
+export const DEFAULT_META_TRANSACTION_BLOCK_HEIGHT_TTL = 100n;
 export const MAX_GAS = 300000000000000n;
 
 export const PAGODA_RPC_ENDPOINTS_MAINNET = [
