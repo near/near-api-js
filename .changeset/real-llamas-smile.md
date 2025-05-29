59d3dc9580be05662cb9a587e82359faccd69d1b
r-near <163825889+r-near@users.noreply.github.com>
2025-05-29 16:08:41 -0700

    fix: ESM Module Resolution (#1559)
    Co-authored-by: Guille <gagdiez.c@gmail.com>

---
+++ .changeset/real-llamas-smile.md
@@ -0,0 +1,19 @@
+---
+"@near-js/accounts": minor
+"@near-js/biometric-ed25519": minor
+"@near-js/client": minor
+"@near-js/cookbook": minor
+"@near-js/crypto": minor
+"@near-js/iframe-rpc": minor
+"@near-js/keystores-browser": minor
+"@near-js/keystores-node": minor
+"@near-js/keystores": minor
+"@near-js/providers": minor
+"@near-js/signers": minor
+"@near-js/tokens": minor
+"@near-js/transactions": minor
+"tsconfig": minor
+"@near-js/types": minor
+---
+
+fix: ESM Module Resolution
---
+++ packages/providers/tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "tsup";
+import { fixFolderImportsPlugin, fixExtensionsPlugin } from 'esbuild-fix-imports-plugin';
+
+export default defineConfig([{
+    splitting: false,
+    bundle: false,
+    entryPoints: ["src/**/*.ts"],
+    format: ["cjs"],
+    outDir: "lib/commonjs",
+    clean: true,
+    dts: true,
+    target: "es2022",
+    esbuildPlugins: [fixFolderImportsPlugin(), fixExtensionsPlugin()],
+}, {
+    splitting: false,
+    bundle: false,
+    entryPoints: ["src/**/*.ts"],
+    format: ["esm"],
+    outDir: "lib/esm",
+    dts: true,
+    clean: true,
+    target: "es2022",
+    esbuildPlugins: [fixFolderImportsPlugin(), fixExtensionsPlugin()],
+}]);
