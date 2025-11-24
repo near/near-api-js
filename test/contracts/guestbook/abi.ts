0b1ce947516ebf84b66da9bc93d3446c2f4f2b89
denbite <mirloxd3@gmail.com>
2025-11-24 20:05:53 +0100

    test: copy integration tests from `e2e` folder into `test/integration`

---
+++ test/contracts/guestbook/abi.ts
@@ -0,0 +1,107 @@
+import type { AbiRoot } from 'near-api-js';
+
+export const abi = {
+    schema_version: '0.4.0',
+    metadata: {
+        name: 'contract',
+        version: '0.1.0',
+        build: {
+            compiler: 'rustc 1.86.0',
+            builder: 'cargo-near cargo-near-build 0.7.2',
+        },
+        wasm_hash: 'EmTQGUyEmTpd6BigmqBtYqZcX9ps8zJ48UPQHSc16vm9',
+    },
+    body: {
+        functions: [
+            {
+                name: 'add_message',
+                kind: 'call',
+                modifiers: ['payable'],
+                params: {
+                    serialization_type: 'json',
+                    args: [
+                        {
+                            name: 'text',
+                            type_schema: {
+                                type: 'string',
+                            },
+                        },
+                    ],
+                },
+            },
+            {
+                name: 'contract_source_metadata',
+                kind: 'view',
+            },
+            {
+                name: 'get_messages',
+                kind: 'view',
+                params: {
+                    serialization_type: 'json',
+                    args: [
+                        {
+                            name: 'from_index',
+                            type_schema: {
+                                type: ['string', 'null'],
+                            },
+                        },
+                        {
+                            name: 'limit',
+                            type_schema: {
+                                type: ['string', 'null'],
+                            },
+                        },
+                    ],
+                },
+                result: {
+                    serialization_type: 'json',
+                    type_schema: {
+                        type: 'array',
+                        items: {
+                            $ref: '#/definitions/PostedMessage',
+                        },
+                    },
+                },
+            },
+            {
+                name: 'total_messages',
+                kind: 'view',
+                result: {
+                    serialization_type: 'json',
+                    type_schema: {
+                        type: 'integer',
+                        format: 'uint32',
+                        minimum: 0.0,
+                    },
+                },
+            },
+        ],
+        root_schema: {
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            title: 'String',
+            type: 'string',
+            definitions: {
+                AccountId: {
+                    description:
+                        'NEAR Account Identifier.\n\nThis is a unique, syntactically valid, human-readable account identifier on the NEAR network.\n\n[See the crate-level docs for information about validation.](index.html#account-id-rules)\n\nAlso see [Error kind precedence](AccountId#error-kind-precedence).\n\n## Examples\n\n``` use near_account_id::AccountId;\n\nlet alice: AccountId = "alice.near".parse().unwrap();\n\nassert!("ƒelicia.near".parse::<AccountId>().is_err()); // (ƒ is not f) ```',
+                    type: 'string',
+                },
+                PostedMessage: {
+                    type: 'object',
+                    required: ['premium', 'sender', 'text'],
+                    properties: {
+                        premium: {
+                            type: 'boolean',
+                        },
+                        sender: {
+                            $ref: '#/definitions/AccountId',
+                        },
+                        text: {
+                            type: 'string',
+                        },
+                    },
+                },
+            },
+        },
+    },
+} as const satisfies AbiRoot;
---
+++ test/integration/typed-contract.test.ts
@@ -0,0 +1,153 @@
+import { readFile } from 'fs/promises';
+import { Worker } from 'near-workspaces';
+import { afterAll, beforeAll, expect, test } from 'vitest';
+import { Account, JsonRpcProvider, KeyPair, KeyPairSigner, type KeyPairString, NEAR, TypedContract } from '../../src';
+import { abi } from '../contracts/guestbook/abi.js';
+import { getRpcUrl, getSecretKey } from './worker.js';
+
+let worker: Worker;
+let rootAccount: Account;
+let guestbookAccount: Account;
+
+beforeAll(async () => {
+    worker = await Worker.init();
+
+    const provider = new JsonRpcProvider({ url: getRpcUrl(worker) });
+    const signer = KeyPairSigner.fromSecretKey(getSecretKey(worker) as KeyPairString);
+
+    rootAccount = new Account(worker.rootAccount.accountId, provider, signer);
+
+    await rootAccount.createAccount(
+        `guestbook.${rootAccount.accountId}`,
+        await signer.getPublicKey(),
+        NEAR.toUnits('10')
+    );
+    guestbookAccount = new Account(`guestbook.${rootAccount.accountId}`, provider, signer);
+
+    const wasm = await readFile('./test/contracts/guestbook/contract.wasm');
+    const tx = await guestbookAccount.deployContract(wasm);
+    await rootAccount.provider.viewTransactionStatus(tx.transaction.hash, guestbookAccount.accountId, 'FINAL');
+});
+
+afterAll(async () => {
+    if (!worker) return;
+
+    await worker.tearDown();
+});
+
+test('TypedContract has abi', async () => {
+    const contract = new TypedContract({
+        contractId: guestbookAccount.accountId,
+        provider: rootAccount.provider,
+        abi: abi,
+    });
+
+    expect(contract.abi).toBe(abi);
+});
+
+test('TypedContract has contractId', async () => {
+    const contract = new TypedContract({
+        contractId: guestbookAccount.accountId,
+        provider: rootAccount.provider,
+        abi: abi,
+    });
+
+    expect(contract.contractId).toBe(guestbookAccount.accountId);
+});
+
+test("TypedContract has view & call properties even if ABI isn't provided", async () => {
+    const contract = new TypedContract({
+        contractId: guestbookAccount.accountId,
+        provider: rootAccount.provider,
+    });
+
+    expect(contract).toHaveProperty('view');
+    expect(contract).toHaveProperty('call');
+});
+
+test("TypedContract doesn't have abi if ABI isn't provided", async () => {
+    const contract = new TypedContract({
+        contractId: guestbookAccount.accountId,
+        provider: rootAccount.provider,
+    });
+
+    expect(contract).not.toHaveProperty('abi');
+});
+
+test("TypedContract doesn't have view & call properties if ABI is empty", async () => {
+    const contract = new TypedContract({
+        contractId: guestbookAccount.accountId,
+        provider: rootAccount.provider,
+        abi: {
+            schema_version: '0.4.0',
+            metadata: {},
+            body: {
+                functions: [],
+                root_schema: {},
+            },
+        },
+    });
+
+    expect(contract.contractId).not.toHaveProperty('view');
+    expect(contract.contractId).not.toHaveProperty('call');
+});
+
+test('TypedContract can invoke a view function', async () => {
+    // TODO: use fixtures for account creation and contract deploy
+    const contractId = `${Date.now()}.${rootAccount.accountId}`;
+    const keypair = KeyPair.fromRandom('ed25519');
+
+    await rootAccount.createAccount(contractId, keypair.getPublicKey(), NEAR.toUnits('10'));
+    const guestbookAccount = new Account(contractId, rootAccount.provider, new KeyPairSigner(keypair));
+
+    const wasm = await readFile('./test/contracts/guestbook/contract.wasm');
+    const tx = await guestbookAccount.deployContract(wasm);
+    await rootAccount.provider.viewTransactionStatus(tx.transaction.hash, guestbookAccount.accountId, 'FINAL');
+
+    const contract = new TypedContract({
+        contractId: guestbookAccount.accountId,
+        provider: rootAccount.provider,
+        abi: abi,
+    });
+
+    const totalMessages = await contract.view.total_messages();
+
+    expect(totalMessages).toBe(0);
+});
+
+test('TypedContract can invoke a call function', async () => {
+    // TODO: use fixtures for account creation and contract deploy
+    const contractId = `${Date.now()}.${rootAccount.accountId}`;
+    const keypair = KeyPair.fromRandom('ed25519');
+
+    await rootAccount.createAccount(contractId, keypair.getPublicKey(), NEAR.toUnits('10'));
+    const guestbookAccount = new Account(contractId, rootAccount.provider, new KeyPairSigner(keypair));
+
+    const wasm = await readFile('./test/contracts/guestbook/contract.wasm');
+    const tx = await guestbookAccount.deployContract(wasm);
+    await rootAccount.provider.viewTransactionStatus(tx.transaction.hash, guestbookAccount.accountId, 'FINAL');
+
+    const contract = new TypedContract({
+        contractId: guestbookAccount.accountId,
+        provider: rootAccount.provider,
+        abi: abi,
+    });
+
+    await contract.call.add_message({
+        args: {
+            text: 'Hello, world!',
+        },
+        deposit: 1n,
+        gas: 30_000_000_000_000n,
+        waitUntil: 'FINAL',
+        account: rootAccount,
+    });
+
+    const messages = await contract.view.get_messages({ args: {} });
+    expect(messages.length).toBe(1);
+    expect(messages.at(0)!).toMatchObject({
+        sender: rootAccount.accountId,
+        text: 'Hello, world!',
+        premium: false,
+    });
+});
