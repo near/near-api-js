ae8f65d09625f9c6847a32647c867f1585b1d6f0
Guillermo Alejandro Gallardo Diez <gagdiez@MacBookPro.home>
2026-01-17 16:31:55 +0100

    chore: add generateSeedPhrase function

--- src/seed-phrase/parse.ts
+++
@@ -1,9 +0,0 @@
-import { parseSeedPhrase as internalParseSeedPhrase } from 'near-seed-phrase';
-import type { KeyPairString } from '../crypto/constants.js';
-import { KeyPair } from '../crypto/key_pair.js';
-
-export function parseSeedPhrase(seedPhrase: string): KeyPair {
-    const { secretKey } = internalParseSeedPhrase(seedPhrase);
-
-    return KeyPair.fromString(secretKey as KeyPairString);
-}
