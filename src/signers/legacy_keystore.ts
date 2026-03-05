68f7eefea6fef12dc013f8c5316978b796facca1
Guillermo Alejandro Gallardo Diez <gagdiez@iR2.local>
2026-03-05 19:59:38 +0100

    fix: lint

--- src/signers/legacy_keystore.ts
+++ src/signers/legacy_keystore.ts
@@ -1,17 +1,17 @@
-import {  KeyPair, type KeyPairString } from '../crypto/index.js';
-import { KeyPairSigner } from './key_pair_signer.js';
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
+import { KeyPair, type KeyPairString } from '../crypto/index.js';
+import { KeyPairSigner } from './key_pair_signer.js';
 
 /**
  * Signs using local credentials stored in the .near-credentials directory.
- * 
+ *
  * @param accountId - The NEAR account ID for which to load credentials.
  * @param networkId - The NEAR network ID (e.g., "mainnet", "testnet"). Defaults to "testnet".
  * @returns A KeyPairSigner instance initialized with the loaded credentials.
  * @throws Will throw an error if the credentials path or file does not exist, or if the file format is invalid.
- * 
+ *
  * @example
  * const signer = new LegacyKeyStore("my-account.testnet");
  * const publicKey = await signer.getPublicKey();
@@ -19,8 +19,7 @@ import path from 'path';
  */
 export class LegacyKeyStoreSigner extends KeyPairSigner {
     constructor(accountId: string, networkId: 'testnet' | 'mainnet' = 'testnet') {
-        
-        let localCredentialsPath = path.join(os.homedir(), '.near-credentials', networkId);
+        const localCredentialsPath = path.join(os.homedir(), '.near-credentials', networkId);
         if (!fs.existsSync(localCredentialsPath)) {
             throw new Error(`Credential path does not exist: ${localCredentialsPath}`);
         }
